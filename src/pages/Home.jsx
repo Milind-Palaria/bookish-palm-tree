59a520988fca1432f4c19eca47e27c9558f7e963
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-05 20:10:45 +0530

    Landing Page #3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,9 +14,17 @@ const Home = () => {
         <div>
             {/* <h1 className="text-8xl mb-10">main</h1> */}
             <div className="h-screen text-[9rem] text-white bg-black lobster-regular flex justify-around underline">
+                <div className="flex flex-col w-1/2 items-center">
                 <h1 className=" translate-y-5">Bookish</h1>
                 <h1 className=" translate-y-5">Palm</h1>
                 <h1 className=" translate-y-5 ">Tree .</h1>
+                </div>
+                <div className=" w-1/2">
+                    <img src="book1.jpg" alt="book" className=" w-4/12 absolute top-16 right-72"/>
+                    <img src="book2.jpg" alt="book"  className=" w-4/12 absolute top-48 right-8"/>
+                    <img src="book3.jpg" alt="book"  className=" w-4/12 absolute top-80"/>
+
+                </div>
             </div>
             <div className="flex gap-10 items-center justify-center">
                 {
