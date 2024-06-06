09f96446d2cadb0bab72322c69e9af0a4fd89d33
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-06 23:45:42 +0530

    Fixes

--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -13,23 +13,23 @@ const Home2 = () => {
     return (
         <div>
             {/* <h1 className="text-8xl mb-10">main</h1> */}
-            <div className="h-screen w-screen overflow-x-hidden bg-black text-white  lobster-regular flex justify-around underline pt-4">
+            <div className="h-screen w-screen overflow-x-hidden bg-black text-white  lobster-regular flex justify-around underline">
                 {/* <div className="flex flex-col w-1/2 items-center">
                 <h1 className=" translate-y-5">Bookish</h1>
                 <h1 className=" translate-y-5">Palm</h1>
                 <h1 className=" translate-y-5 ">Tree .</h1>
                 </div> */}
-                <div className=" w-full m-auto h-screen">
+                <div className=" w-full h-full mt-4">
 
-                    <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem]">Bookish <span className=" text-blue-900 mix-blend-normal">Bookish</span> Bookish</h1>
+                    <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem] translate-y-[-1rem]">Bookish</h1>
                     
                     <img src="book1.jpg" alt="book" className=" w-[80%] h-1/4 m-auto object-cover opacity-50"/>
 
-                    <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem]">Palm <span className=" text-blue-900 mix-blend-normal">Palm</span> Palm</h1>
+                    <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem] translate-y-6">Palm</h1>
                     
                     <img src="book2.jpg" alt="book"  className=" w-[80%] h-1/4  m-auto mt-10 object-cover opacity-50" />
                     
-                    <h1 className="absolute mix-blend-normal left-0 right-0 text-center text-[8rem]">tree <span className=" text-blue-900 mix-blend-normal">tree</span> tree </h1>
+                    <h1 className="absolute mix-blend-normal left-0 right-0 text-center text-[8rem] translate-y-4">tree</h1>
                     
                     <img src="book3.jpg" alt="book"  className=" w-[80%] h-1/4  m-auto mt-10 object-cover opacity-50"/>
 
