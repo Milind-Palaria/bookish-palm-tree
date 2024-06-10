5e216c5d36de050fb4389741dc4cb67e69f627f5
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-10 22:00:24 +0530

    Navbar changes #2 GSAP Initialization

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import logo from "../assests/bookish-logo.png"
+import { gsap } from "gsap/dist/gsap";
+import { useGSAP } from "@gsap/react/dist";
+
+gsap.registerPlugin(useGSAP);
+ 
 
 const Nav = () => {
+  
+useGSAP(()=>{
+  gsap.to('.nav-gsap',{
+    duration:1,
+    opacity:1,
+    delay:4,
+    ease:'power1.in',
+    stagger:{
+      amount:1,
+    }
+  })
+},[]);
+
   return (
     <div className=' sticky bg-black w-full top-0 p-2 flex justify-center backdrop-blur-md text-white z-50'>
       {/* <NavLink to="/"><img src={logo} alt="logo" className=' w-24'/></NavLink> */}
@@ -15,18 +33,18 @@ const Nav = () => {
 
       <div className='flex gap-20 w-1/2 justify-center text-center lobster-regular'>
 
-        <NavLink to="/register" className='hover:tracking-widest duration-500  w-1/4'>
+        <NavLink to="/register" className='hover:tracking-widest duration-500  w-1/4 opacity-0 nav-gsap'>
         <h1 className='bg-white/80 text-[#000] hover:bg-white rounded-sm'>
           Register
           </h1>
         </NavLink>
-        <NavLink to="/login"  className='hover:tracking-widest duration-500  w-1/4' > <h1 className='bg-white/80 text-[#000] hover:bg-white rounded-sm'>
+        <NavLink to="/login"  className='hover:tracking-widest duration-500  w-1/4 opacity-0 nav-gsap' > <h1 className='bg-white/80 text-[#000] hover:bg-white rounded-sm'>
         Login
           </h1></NavLink>
-        <NavLink to="/book/list"  className='hover:tracking-widest duration-500  w-1/4' > <h1 className=' bg-white/80 text-[#000] hover:bg-white rounded-sm'>
+        <NavLink to="/book/list"  className='hover:tracking-widest duration-500  w-1/4 opacity-0 nav-gsap' > <h1 className=' bg-white/80 text-[#000] hover:bg-white rounded-sm'>
           List
           </h1></NavLink>
-        <NavLink to="/book/orders"  className='hover:tracking-widest duration-500  w-1/4' > <h1 className='bg-white/80 text-[#000] hover:bg-white rounded-sm'>
+        <NavLink to="/book/orders"  className='hover:tracking-widest duration-500  w-1/4 opacity-0 nav-gsap' > <h1 className='bg-white/80 text-[#000] hover:bg-white rounded-sm'>
           View Orders
           </h1></NavLink>
         {/* <NavLink to="/book/orders">View Order</NavLink> */}
