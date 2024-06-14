6b6ea880b175a4a87f32da94cf116c9f38f0d182
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-14 23:30:48 +0530

    New Card Component added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useFirebase } from '../context/Firebase'
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 import { gsap } from "gsap/dist/gsap";
 import { useGSAP } from "@gsap/react/dist";
-import {ScrollTrigger} from 'gsap/all'
+import { ScrollTrigger } from 'gsap/all'
 import { useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -12,44 +12,57 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(useGSAP);
 
 const Card = (props) => {
-    const firebase=useFirebase()
-    const navigate= useNavigate()
-    const [url,setURL]=useState(null)
+    const firebase = useFirebase()
+    const navigate = useNavigate()
+    const [url, setURL] = useState(null)
     useEffect(() => {
-      firebase.getImageURL(props.imageURL).then((url)=>setURL(url));
+        firebase.getImageURL(props.imageURL).then((url) => setURL(url));
     }, [])
 
-    
-  const scrollRef=useRef();
 
-  useGSAP(()=>{
-    const boxes=gsap.utils.toArray(scrollRef.current.children);
+    const scrollRef = useRef();
 
-    boxes.forEach((box)=>{
-        gsap.fromTo(box,{
-          scale:0,
-          opacity:0,
-        },{
-            scale:1,
-            opacity:1,
-            scrollTrigger:{
-              trigger:box,
-              start:"top bottom",
-              end:"top 50%",
-              scrub:1
-            },
-            ease:"power1.inOut"
-          }
-    )
-      })
-    },[{scope:scrollRef}]);
+    useGSAP(() => {
+        const boxes = gsap.utils.toArray(scrollRef.current.children);
 
-    
+        boxes.forEach((box) => {
+            gsap.fromTo(box, {
+                scale: 0,
+                opacity: 0,
+            }, {
+                scale: 1,
+                opacity: 1,
+                scrollTrigger: {
+                    trigger: box,
+                    start: "top bottom",
+                    end: "top 50%",
+                    scrub: 1
+                },
+                ease: "power1.inOut"
+            }
+            )
+        })
+    }, [{ scope: scrollRef }]);
+
+
     return (
-        <div  ref={scrollRef}>
+        <div ref={scrollRef}>
+            <div className="swiper-slide">
+                <img src={url} />
+                <div className="overlay">
+                    <h1>Walking Tour in Florence</h1>
+                    <p>
+                        Discover the fascinating beauty of this historic city by
+                        strolling through the rich cultural tapestry that makes Florence
+                        a timeless destination.
+                    </p>
 
-        <div  className="flex flex-wrap gap-2 items-center justify-center p-10">
+                </div>
+            </div>
+            {/* <div  className="flex flex-wrap gap-2 items-center justify-center p-10 bg-white text-black">
             <img src={url} className=' h-40 w-40'/>
+            <div>
+
             <h1>Book Details</h1>
             <h2>{props.name}</h2>
             <h3>Title: {props.name}</h3>
@@ -57,7 +70,9 @@ const Card = (props) => {
             <h3>ISBN Number: {props.isbn}</h3>
             <p>Price: {props.price}</p>
             <button onClick={e=>navigate(props.link)}>Explore</button>
-        </div>
+            </div>
+        </div> */}
+
         </div>
     )
 }
--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -1,7 +1,7 @@
 
 import { gsap } from "gsap/dist/gsap";
 import { useGSAP } from "@gsap/react/dist";
-import {ScrollTrigger} from 'gsap/all'
+import { ScrollTrigger } from 'gsap/all'
 import { useRef } from "react";
 import CurrentBooks from "./CurrentBooks";
 
@@ -10,128 +10,129 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(useGSAP);
 const Home2 = () => {
 
-  const scrollRef=useRef();
+  const scrollRef = useRef();
 
 
-    useGSAP(()=>{
-        gsap.to('.text-gsap',{
-          y:0,
-          duration:1,
-          opacity:1,
-          delay:2,
-          ease:'power1.in',
-          stagger:{
-            amount:1,
-          }
-        })
-      },[]);
+  useGSAP(() => {
+    gsap.to('.text-gsap', {
+      y: 0,
+      duration: 1,
+      opacity: 1,
+      delay: 2,
+      ease: 'power1.in',
+      stagger: {
+        amount: 1,
+      }
+    })
+  }, []);
 
-    useGSAP(()=>{
-        gsap.to('.img-gsap',{
-          duration:2,
-          opacity:0.5,
-          ease:'power1.in',
-          stagger:{
-            amount:1,
-          }
-        })
-      },[]);
-      useGSAP(()=>{
-        gsap.to('.text-gsap',{
-          duration:1,
-          filter:'blur(10px)',
-          color:'#000',
-          letterSpacing:'30px',
-          scrollTrigger:{
-            trigger:'.text-gsap',
-            start:"+=1",
-            scrub:1
-          },
-          ease:'power1.in',
-          stagger:{
-            amount:1,
-          }
-        })
-      },[{scope:scrollRef}]);
-      useGSAP(()=>{
-        gsap.to('.img1-gsap',{
-          duration:10,
-          width:"100%",
-          filter:'blur(10px)',
-          scrollTrigger:{
-            trigger:'.img-gsap',
-            start:"+=1",
-            markers:true,
-            scrub:1
-          },
-          ease:'power1.in',
-          stagger:{
-            amount:1,
-          }
-        })
-      },[{scope:scrollRef}]);
-      useGSAP(()=>{
-        gsap.to('.img2-gsap',{
-          duration:10,
-          width:"100%",
-          filter:'blur(10px)',
-          scrollTrigger:{
-            trigger:'.img-gsap',
-            start:"+=1",
-            markers:true,
-            scrub:1
-          },
-          ease:'power1.in',
-          stagger:{
-            amount:1,
-          }
-        })
-      },[{scope:scrollRef}]);
-      useGSAP(()=>{
-        gsap.to('.img3-gsap',{
-          duration:10,
-          width:"100%",
-          filter:'blur(10px)',
-          scrollTrigger:{
-            trigger:'.img-gsap',
-            start:"+=1",
-            markers:true,
-            scrub:1
-          },
-          ease:'power1.in',
-          stagger:{
-            amount:1,
-          }
-        })
-      },[{scope:scrollRef}]);
-    return (
-        <div>
-            <div className="h-screen w-screen overflow-x-hidden bg-black text-white  lobster-regular flex justify-around underline overflow-y-hidden">
-                {/* <div className="flex flex-col w-1/2 items-center">
+  useGSAP(() => {
+    gsap.to('.img-gsap', {
+      duration: 2,
+      opacity: 0.5,
+      ease: 'power1.in',
+      stagger: {
+        amount: 1,
+      }
+    })
+  }, []);
+  useGSAP(() => {
+    gsap.to('.text-gsap', {
+      duration: 1,
+      filter: 'blur(10px)',
+      color: '#000',
+      letterSpacing: '30px',
+      scrollTrigger: {
+        trigger: '.text-gsap',
+        start: "+=1",
+        scrub: 1
+      },
+      ease: 'power1.in',
+      stagger: {
+        amount: 1,
+      }
+    })
+  }, [{ scope: scrollRef }]);
+  useGSAP(() => {
+    gsap.to('.img1-gsap', {
+      duration: 10,
+      width: "100%",
+      filter: 'blur(10px)',
+      scrollTrigger: {
+        trigger: '.img-gsap',
+        start: "+=1",
+        markers: true,
+        scrub: 1
+      },
+      ease: 'power1.in',
+      stagger: {
+        amount: 1,
+      }
+    })
+  }, [{ scope: scrollRef }]);
+  useGSAP(() => {
+    gsap.to('.img2-gsap', {
+      duration: 10,
+      width: "100%",
+      filter: 'blur(10px)',
+      scrollTrigger: {
+        trigger: '.img-gsap',
+        start: "+=1",
+        markers: true,
+        scrub: 1
+      },
+      ease: 'power1.in',
+      stagger: {
+        amount: 1,
+      }
+    })
+  }, [{ scope: scrollRef }]);
+  useGSAP(() => {
+    gsap.to('.img3-gsap', {
+      duration: 10,
+      width: "100%",
+      filter: 'blur(10px)',
+      scrollTrigger: {
+        trigger: '.img-gsap',
+        start: "+=1",
+        markers: true,
+        scrub: 1
+      },
+      ease: 'power1.in',
+      stagger: {
+        amount: 1,
+      }
+    })
+  }, [{ scope: scrollRef }]);
+  return (
+    <div>
+      <div className="h-screen w-screen overflow-x-hidden bg-black text-white  lobster-regular flex justify-around underline overflow-y-hidden">
+        {/* <div className="flex flex-col w-1/2 items-center">
                 <h1 className=" translate-y-5">Bookish</h1>
                 <h1 className=" translate-y-5">Palm</h1>
                 <h1 className=" translate-y-5 ">Tree .</h1>
                 </div> */}
-                <div className=" w-full h-full mt-4  overflow-y-hidden">
+        <div className=" w-full h-full mt-4  overflow-y-hidden">
 
-                    <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem] mt-[-10] text-gsap opacity-0">— Bookish —</h1>
-                    
-                    <img src="book1.jpg" alt="book" className=" w-[80%] h-1/4 m-auto object-cover img-gsap  img1-gsap opacity-0"/>
+          <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem] mt-[-10] text-gsap opacity-0">— Bookish —</h1>
 
-                    <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem] mt-7 text-gsap opacity-0">— Palm — </h1>
-                    
-                    <img src="book2.jpg" alt="book"  className=" w-[80%] h-1/4  m-auto mt-10 object-cover img-gsap img2-gsap opacity-0" />
-                    
-                    <h1 className="absolute mix-blend-normal left-0 right-0 text-center text-[8rem] mt-5 text-gsap opacity-0">— tree — </h1>
-                    
-                    <img src="book3.jpg" alt="book"  className=" w-[80%] h-1/4  m-auto mt-10 object-cover img-gsap img3-gsap opacity-0"/>
+          <img src="book1.jpg" alt="book" className=" w-[80%] h-1/4 m-auto object-cover img-gsap  img1-gsap opacity-0" />
 
-                </div>
-            </div>
-                <CurrentBooks/>
-           
+          <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem] mt-7 text-gsap opacity-0">— Palm — </h1>
+
+          <img src="book2.jpg" alt="book" className=" w-[80%] h-1/4  m-auto mt-10 object-cover img-gsap img2-gsap opacity-0" />
+
+          <h1 className="absolute mix-blend-normal left-0 right-0 text-center text-[8rem] mt-5 text-gsap opacity-0">— tree — </h1>
+
+          <img src="book3.jpg" alt="book" className=" w-[80%] h-1/4  m-auto mt-10 object-cover img-gsap img3-gsap opacity-0" />
+
         </div>
-    )
+      </div>
+   
+      <CurrentBooks />
+
+    </div>
+  )
 }
 
 export default Home2
