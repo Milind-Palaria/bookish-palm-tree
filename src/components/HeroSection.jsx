eeac4b6bdb098430dacced8b8cc8e7f710fd1baf
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-23 23:55:38 +0530

    Contents Of hero section #2

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,21 +2,12 @@
 const HeroSection = () => {
   return (
     <div className="h-screen">
-      <div className=" h-full text-5xl">
-        <div className=" w-screen flex  flex-col items-center justify-center gap-20">
-        <div className="left-0">
-        <img src="hero-1.jpg" alt="Hero Image" className=" w-[60%] absolute left-[14vw]"  data-scroll data-scroll-speed="0.4"/>
-        <img src="hero-2.jpg" alt="Hero Image" className=" w-[60%] absolute left-[20vw]"  data-scroll data-scroll-speed="0.7"/>
-        <img src="hero-3.jpg" alt="Hero Image" className=" w-[60%] absolute left-[8vw]"  data-scroll data-scroll-speed="1"/>
-        </div>
-        <div className="absolute right-0">
-          <h1>Register</h1>
-          <h1>Login</h1>
-          <h1>List</h1>
-          <h1>View Orders</h1>
-
-        </div>
-        </div>
+      <div className=" h-full relative flex flex-col items-center justify-center text-5xl">
+        <img src="hero-1.jpg" alt="Hero Image" className=" w-[35%] absolute left-[14vw] top-[5vh]"  data-scroll data-scroll-speed="0.4"/>
+        <img src="hero-2.jpg" alt="Hero Image" className=" w-[35%] absolute left-[20vw] top-[15vh]"  data-scroll data-scroll-speed="0.7"/>
+        <img src="hero-3.jpg" alt="Hero Image" className=" w-[35%] absolute left-[8vw] top-[20vh]"  data-scroll data-scroll-speed="1"/>
+         <h1 className="absolute right-[10vw] top-0"  data-scroll data-scroll-speed="0.7">Hello</h1>
+        <h1 className="absolute right-[10vw] top-[10vh]"  data-scroll data-scroll-speed="1" >Landing</h1>
 {/* 
       <h1 className="my-4" data-scroll data-scroll-speed="0.4">Hero Section</h1>
       <h1  className="my-4" data-scroll data-scroll-speed="0.6">second</h1>
