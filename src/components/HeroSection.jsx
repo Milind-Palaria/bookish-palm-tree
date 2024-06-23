f7d85c28fd59e6975aff1ff7e70e5a12fb54c4b1
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-24 00:00:01 +0530

    Code Removal

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,10 +11,7 @@ const HeroSection = () => {
         <h1 className="absolute right-[10vw] top-[80vh]"  data-scroll data-scroll-speed="0.9" >List</h1>
         <h1 className="absolute right-[10vw] top-[85vh]" data-scroll data-scroll-speed="1" >View Order</h1>
 
-{/* 
-      <h1 className="my-4" data-scroll data-scroll-speed="0.4">Hero Section</h1>
-      <h1  className="my-4" data-scroll data-scroll-speed="0.6">second</h1>
-      <h1  className="my-4" data-scroll data-scroll-speed="1">Third</h1> */}
+
       </div>
     </div>
   )
