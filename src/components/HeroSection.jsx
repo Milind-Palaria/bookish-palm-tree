b094a9a0895c06e4d6e590d2614f8ad3d68bff7c
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-24 04:37:34 +0530

    Contents Of hero section #6

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,19 +1,22 @@
 
 const HeroSection = () => {
+ 
+
   return (
     <div className="h-[80vh] relative">
 
       <div className="h-full relative text-5xl top-[5vh]">
-        <img src="hero-1.jpg" alt="Hero Image" className=" w-[35%] absolute left-[14vw] top-[5vh] rounded-lg" data-scroll data-scroll-speed="0.5" />
-        <img src="hero-2.jpg" alt="Hero Image" className=" w-[35%] absolute left-[20vw] top-[15vh] rounded-lg" data-scroll data-scroll-speed="0.8" />
-        <img src="hero-3.jpg" alt="Hero Image" className=" w-[35%] absolute left-[8vw] top-[20vh] rounded-lg" data-scroll data-scroll-speed="1.2" />
-        <div className="absolute right-0 flex flex-col top-[23vh]">
+        <img src="hero-1.jpg" alt="Hero Image" className=" w-[35%] absolute left-[17vw] top-[5vh] rounded-lg" data-scroll data-scroll-speed="0.5" />
+        <img src="hero-2.jpg" alt="Hero Image" className=" w-[35%] absolute left-[25vw] top-[15vh] rounded-lg" data-scroll data-scroll-speed="0.8" />
+        <img src="hero-3.jpg" alt="Hero Image" className=" w-[35%] absolute left-[12vw] top-[20vh] rounded-lg" data-scroll data-scroll-speed="1.2" />
+        <div className="absolute right-20 flex flex-col top-[23vh]">
 
-        <h1 className="mt-0" data-scroll data-scroll-speed="0.7">Register</h1>
-        <h1 className="mt-0"  data-scroll data-scroll-speed="0.9" >Login</h1>
-        <h1 className=" mt-[-0.5rem]"  data-scroll data-scroll-speed="1.1" >List</h1>
-        <h1 className=" mt-[-1rem]"  data-scroll data-scroll-speed="1.3" >View Order</h1>
+        <h1 className="mt-0 opacity-60" data-scroll data-scroll-speed="0.7">Register</h1>
+        <h1 className="mt-0 opacity-70"  data-scroll data-scroll-speed="0.9" >Login</h1>
+        <h1 className=" mt-[-0.5rem] opacity-80"  data-scroll data-scroll-speed="1.1" >List</h1>
+        <h1 className=" mt-[-1rem] opacity-90"  data-scroll data-scroll-speed="1.3" >View Order</h1>
         </div>
+        <h1 className="absolute w-screen text-8xl bottom-0 m-auto text-center best-text">Bestsellers</h1>
         {/* <div className="absolute right-0 top-[20vh]"> 
           <h1 data-scroll data-scroll-speed="0.7">aslcamlsk</h1>
           <h1  data-scroll data-scroll-speed="0.8">aslcamlsk</h1>
@@ -21,7 +24,6 @@ const HeroSection = () => {
           <h1 data-scroll data-scroll-speed="1">aslcamlsk</h1>
 
         </div> */}
-        {/* <h1>Bookish Palm Tree</h1> */}
 
       </div>
     </div>
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -108,15 +108,15 @@ const LandingPage = () => {
                 </div> */}
         <div className=" w-full h-full mt-4  overflow-y-hidden">
 
-          <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem] mt-[-10] text-gsap opacity-0">— Bookish —</h1>
+          <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem] mt-[-10] text-gsap opacity-0 tracking-wide">— Bookish —</h1>
 
           <img src="book1.jpg" alt="book" className=" w-[80%] h-1/4 m-auto object-cover img-gsap  img1-gsap opacity-0" />
 
-          <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem] mt-7 text-gsap opacity-0">— Palm — </h1>
+          <h1 className="absolute mix-blend-normal text-nowrap left-0 right-0 text-center text-[8rem] mt-7 text-gsap opacity-0 tracking-wide">— Palm — </h1>
 
           <img src="book2.jpg" alt="book" className=" w-[80%] h-1/4  m-auto mt-10 object-cover img-gsap img2-gsap opacity-0" />
 
-          <h1 className="absolute mix-blend-normal left-0 right-0 text-center text-[8rem] mt-5 text-gsap opacity-0">— tree — </h1>
+          <h1 className="absolute mix-blend-normal left-0 right-0 text-center text-[8rem] mt-5 text-gsap opacity-0 tracking-wide">— tree — </h1>
 
           <img src="book3.jpg" alt="book" className=" w-[80%] h-1/4  m-auto mt-10 object-cover img-gsap img3-gsap opacity-0" />
 
