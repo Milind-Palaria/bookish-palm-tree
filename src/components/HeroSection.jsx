fd29fd83aae341896e55e7c4c7d705849d88a242
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-23 23:47:22 +0530

    Contents Of hero section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,10 +2,25 @@
 const HeroSection = () => {
   return (
     <div className="h-screen">
-      <div className=" h-full flex flex-col items-center justify-center text-8xl">
+      <div className=" h-full text-5xl">
+        <div className=" w-screen flex  flex-col items-center justify-center gap-20">
+        <div className="left-0">
+        <img src="hero-1.jpg" alt="Hero Image" className=" w-[60%] absolute left-[14vw]"  data-scroll data-scroll-speed="0.4"/>
+        <img src="hero-2.jpg" alt="Hero Image" className=" w-[60%] absolute left-[20vw]"  data-scroll data-scroll-speed="0.7"/>
+        <img src="hero-3.jpg" alt="Hero Image" className=" w-[60%] absolute left-[8vw]"  data-scroll data-scroll-speed="1"/>
+        </div>
+        <div className="absolute right-0">
+          <h1>Register</h1>
+          <h1>Login</h1>
+          <h1>List</h1>
+          <h1>View Orders</h1>
+
+        </div>
+        </div>
+{/* 
       <h1 className="my-4" data-scroll data-scroll-speed="0.4">Hero Section</h1>
       <h1  className="my-4" data-scroll data-scroll-speed="0.6">second</h1>
-      <h1  className="my-4" data-scroll data-scroll-speed="1">Third</h1>
+      <h1  className="my-4" data-scroll data-scroll-speed="1">Third</h1> */}
       </div>
     </div>
   )
