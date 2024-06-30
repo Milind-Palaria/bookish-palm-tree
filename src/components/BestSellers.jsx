d5b19f3f58bd0326486b11501e906c549a0d22a4
milind-palaria <lit2021047@iiitl.ac.in>
2024-06-30 23:12:59 +0530

    Bestsellers

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const BestSellers = () => {
   return (
-    <div>BestSellers</div>
+    <div>
+      <div className="flex gap-20 items-center justify-center flex-wrap bg-black text-white "  >
+                {
+                    books && books.map((book) => (
+                        <Card link={`/book/view/${book.id}`} key={book.id} id={book.id} {...book.data()} />
+                    ))
+                }
+            </div>
+    </div>
   )
 }
 
