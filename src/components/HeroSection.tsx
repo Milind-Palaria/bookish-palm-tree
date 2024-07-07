87f0db1ee9fac736b359af90a6b4e4510008e4ed
milind-palaria <lit2021047@iiitl.ac.in>
2024-07-07 23:51:35 +0530

    Link Routes Activate

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+import { Link } from 'react-router-dom';
 
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
@@ -30,18 +31,26 @@ const HeroSection: React.FC = () => {
         <img src="hero-2.jpg" alt="Hero Image" className="w-[35%] absolute left-[25vw] top-[15vh] rounded-lg z-10" data-scroll data-scroll-speed="0.8" />
         <img src="hero-3.jpg" alt="Hero Image" className="w-[35%] absolute left-[12vw] top-[20vh] rounded-lg z-10" data-scroll data-scroll-speed="1.2" />
         <div className="absolute right-20 flex flex-col top-[23vh] heass">
+         <Link to="/register">
           <h1 className="mt-0 opacity-60" data-scroll data-scroll-speed="0.7">
             Register
           </h1>
+         </Link>
+         <Link to="/login">
           <h1 className="mt-0 opacity-70" data-scroll data-scroll-speed="0.9">
             Login
           </h1>
+         </Link>
+         <Link to="/book/list">
           <h1 className="mt-[-0.5rem] opacity-80" data-scroll data-scroll-speed="1.1">
             List
           </h1>
+         </Link>
+         <Link to="/book/orders">
           <h1 className="mt-[-1rem] opacity-90" data-scroll data-scroll-speed="1.3">
             View Order
           </h1>
+         </Link>
         </div>
         <h1 className="absolute w-screen text-8xl bottom-0 m-auto text-center best-text opacity-0 z-0 translate-y-[-2rem]">Bestsellers</h1>
       </div>
